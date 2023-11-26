f5d8ac3d578c727daf7afc3e08f23f6a7410ac54
Alex Boylan <aboylan@users.noreply.github.com>
2023-11-25 21:23:12 -0600

    Creando controller parte 2

--- src/main/java/org/aboylan/test/springboot/app/controllers/CuentaController.java
+++ src/main/java/org/aboylan/test/springboot/app/controllers/CuentaController.java
@@ -1,14 +1,17 @@
 package org.aboylan.test.springboot.app.controllers;
 
 import org.aboylan.test.springboot.app.models.Cuenta;
+import org.aboylan.test.springboot.app.models.TransaccionDTO;
 import org.aboylan.test.springboot.app.services.CuentaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api/cuentas")
@@ -23,4 +26,17 @@ public class CuentaController {
         return cuentaService.findById(id);
     }
 
+    @PostMapping("/transferir")
+    public ResponseEntity<?> transferir(@RequestBody TransaccionDTO dto) {
+        cuentaService.transferir(dto.getCuentaOrigenId(), dto.getCuentaDestinoId(),
+                dto.getMonto(), dto.getBancoId());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status", "OK");
+        response.put("mensaje", "Transferencia realizada con exito");
+        response.put("transaccion", dto);
+
+        return ResponseEntity.ok(response);
+    }
 }
