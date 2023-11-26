de646d754969e4f9766b2ba60f95f74ea547bb18
Alex Boylan <aboylan@users.noreply.github.com>
2023-11-25 21:02:10 -0600

    Creando controller

---
+++ src/main/java/org/aboylan/test/springboot/app/controllers/CuentaController.java
@@ -0,0 +1,26 @@
+package org.aboylan.test.springboot.app.controllers;
+
+import org.aboylan.test.springboot.app.models.Cuenta;
+import org.aboylan.test.springboot.app.services.CuentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/api/cuentas")
+public class CuentaController {
+
+    @Autowired
+    private CuentaService cuentaService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Cuenta detalle(@PathVariable Long id) {
+        return cuentaService.findById(id);
+    }
+
+}
