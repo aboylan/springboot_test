f58050b2038082810b2b83911447116a44566a06
Alex Boylan <aboylan@users.noreply.github.com>
2023-11-28 19:35:21 -0600

    Ejecutando test con Cobertura de codigo (Code Coverage)

--- src/test/java/org/aboylan/test/springboot/app/controllers/CuentaControllerTest.java
+++ src/test/java/org/aboylan/test/springboot/app/controllers/CuentaControllerTest.java
@@ -16,6 +16,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -63,6 +65,16 @@ class CuentaControllerTest {
         dto.setMonto(new BigDecimal("100"));
         dto.setBancoId(1L);
 
+        System.out.println(objectMapper.writeValueAsString(dto));
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status", "OK");
+        response.put("mensaje", "Transferencia realizada con exito");
+        response.put("transaccion", dto);
+
+        System.out.println(objectMapper.writeValueAsString(response));
+
         // When
         mvc.perform(post("/api/cuentas/transferir")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -73,7 +85,8 @@ class CuentaControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.date").value(LocalDate.now().toString()))
                 .andExpect(jsonPath("$.mensaje").value("Transferencia realizada con exito"))
-                .andExpect(jsonPath("$.transaccion.cuentaOrigenId").value(dto.getCuentaOrigenId()));
+                .andExpect(jsonPath("$.transaccion.cuentaOrigenId").value(dto.getCuentaOrigenId()))
+                .andExpect(content().json(objectMapper.writeValueAsString(response)));
 
     }
 }
