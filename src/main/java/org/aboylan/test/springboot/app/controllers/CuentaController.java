cb940d94990b8ffd41d5a224e0edeef46d29a0f9
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-01 18:36:53 -0600

    Escribiendo test de integracion para el eliminar

--- src/main/java/org/aboylan/test/springboot/app/controllers/CuentaController.java
+++ src/main/java/org/aboylan/test/springboot/app/controllers/CuentaController.java
@@ -52,4 +52,11 @@ public class CuentaController {
 
         return ResponseEntity.ok(response);
     }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminar(@PathVariable Long id) {
+        cuentaService.deleteById(id);
+    }
+
 }
