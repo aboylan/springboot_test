1f6738f2d91b58cfd3f6fe3874b92f635abe8d8e
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-05 19:20:11 -0600

    Escribiendo test de integracion para el listar

--- src/test/java/org/aboylan/test/springboot/app/controllers/CuentaControllerTestRestTemplateTest.java
+++ src/test/java/org/aboylan/test/springboot/app/controllers/CuentaControllerTestRestTemplateTest.java
@@ -16,7 +16,9 @@ import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -83,10 +85,37 @@ class CuentaControllerTestRestTemplateTest {
         assertEquals(HttpStatus.OK, respuesta.getStatusCode());
         assertEquals(MediaType.APPLICATION_JSON, respuesta.getHeaders().getContentType());
         assertNotNull(cuenta);
+
         assertEquals(1L, cuenta.getId());
         assertEquals("Alex", cuenta.getPersona());
         assertEquals("900.00", cuenta.getSaldo().toPlainString());
         assertEquals(new Cuenta(1L, "Alex", new BigDecimal("900.00")), cuenta);
+    }
+
+    @Test
+    @Order(3)
+    void testListar() throws JsonProcessingException {
+        ResponseEntity<Cuenta[]> respuesta = client.getForEntity(crearUri("/api/cuentas"), Cuenta[].class);
+        List<Cuenta> cuentas = Arrays.asList(respuesta.getBody());
+
+        assertEquals(HttpStatus.OK, respuesta.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, respuesta.getHeaders().getContentType());
+
+        assertEquals(2, cuentas.size());
+        assertEquals(1L, cuentas.get(0).getId());
+        assertEquals("Alex", cuentas.get(0).getPersona());
+        assertEquals("900.00", cuentas.get(0).getSaldo().toPlainString());
+        assertEquals(2L, cuentas.get(1).getId());
+        assertEquals("John", cuentas.get(1).getPersona());
+        assertEquals("2100.00", cuentas.get(1).getSaldo().toPlainString());
+
+        JsonNode json = objectMapper.readTree(objectMapper.writeValueAsString(cuentas));
+        assertEquals(1L, json.get(0).path("id").asLong());
+        assertEquals("Alex", json.get(0).path("persona").asText());
+        assertEquals("900.0", json.get(0).path("saldo").asText());
+        assertEquals(2L, json.get(1).path("id").asLong());
+        assertEquals("John", json.get(1).path("persona").asText());
+        assertEquals("2100.0", json.get(1).path("saldo").asText());
     }
 
     private String crearUri(String uri) {
